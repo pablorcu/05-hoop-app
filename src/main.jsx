7d4fd4a99527e1f542d6b6a05e29bf5748a422f1
PCampoverde <pcampoverde.dtcee@gmail.com>
2023-01-03 10:01:54 -0500

    01-useState CounterApp

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// import { HooksApp } from './HooksApp';
+import { CounterApp } from './01-useState/CounterApp';
+
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <CounterApp />
   </React.StrictMode>,
 )
